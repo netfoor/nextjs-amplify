f03dcfcb8af1123ed67d8f6d4c135f8b9b830f40
Fortino Romero <2311080254@alumno.utpuebla.edu.mx>
2025-05-12 10:52:49 -0600

    Configure Data resourse using schemas to create tables and relationships in dynamoDB

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,17 +1,29 @@
 import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
 
-/*== STEP 1 ===============================================================
-The section below creates a Todo database table with a "content" field. Try
-adding a new "isDone" field as a boolean. The authorization rule below
-specifies that any unauthenticated user can "create", "read", "update", 
-and "delete" any "Todo" records.
-=========================================================================*/
 const schema = a.schema({
-  Todo: a
+  Post: a
     .model({
-      content: a.string(),
+      title: a.string().required(),
+      content: a.string().required(),
+      owner: a.string().required(),
+      comments: a.hasMany('Comment', 'postId'),
+  })
+    .authorization((allow) => [
+      allow.publicApiKey().to(["read"]),
+      allow.owner(),
+    ]),
+
+  Comment: a
+    .model({
+      content: a.string().required(),
+      post: a.belongsTo('Post', 'postId'),
+      postId: a.id().required(),
+      owner: a.string().required(),
     })
-    .authorization((allow) => [allow.guest()]),
+    .authorization((allow) => [
+      allow.publicApiKey().to(["read"]),
+      allow.owner(),
+    ]),
 });
 
 export type Schema = ClientSchema<typeof schema>;
@@ -19,7 +31,11 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: 'identityPool',
+    defaultAuthorizationMode: 'userPool',
+    apiKeyAuthorizationMode: {
+      expiresInDays: 30,
+      description: 'API key for public access',
+    }
   },
 });
 
